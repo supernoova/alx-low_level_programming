9eed8134b44be5464bd62af4c95359630d9b8292
supernoova <abdellatiffitah@gmail.com>
2023-09-07 15:46:25 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -8,4 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
